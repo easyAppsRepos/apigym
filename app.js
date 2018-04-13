f3ebc2863f08280b27462002f969fc8b55f850a1
easyAppsRepos <jralfarog@gmail.com>
2018-04-13 15:29:36 -0600

    multifechas

--- app.js
+++ app.js
@@ -437,7 +437,7 @@ var new_date = moment(datePartido[0]).add((7*(i+1)), 'days');
 
 
 
-console.log(moment(datePartido[0]).add((7*(i+1)), 'days'));
+
 
 var new_date2 =moment.unix(new_date).format("YYYY-MM-DD")+' '+datePartido[1];
 
